449fc8c96d5a9882395e16c94d075840ff8e575e
Dave Batiste <dave.batiste@d2l.com>
2020-09-21 11:12:21 -0400

    Linting.

--- icons.js
+++ icons.js
@@ -4,8 +4,8 @@ const icons = {
 	'resize-handle': accessibilityCheck
 };
 
-Object.keys(icons).forEach((key,index) => {
-	icons[key] = icons[key].replace( /fill="#494c4e"/g, 'fill-rule="nonzero"');
+Object.keys(icons).forEach((key) => {
+	icons[key] = icons[key].replace(/fill="#494c4e"/g, 'fill-rule="nonzero"');
 });
 
 export { icons };
