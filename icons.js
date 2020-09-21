097e7e1794de9db1868e1f3d7a2ecb76ce4e26f5
Dave Batiste <dave.batiste@d2l.com>
2020-09-21 11:03:58 -0400

    Enable the ability to resize the editor using the resize handle.

---
+++ icons.js
@@ -0,0 +1,11 @@
+import { val as accessibilityCheck } from '@brightspace-ui/core/generated/icons/tier1/resize-right.js';
+
+const icons = {
+	'resize-handle': accessibilityCheck
+};
+
+Object.keys(icons).forEach((key,index) => {
+	icons[key] = icons[key].replace( /fill="#494c4e"/g, 'fill-rule="nonzero"');
+});
+
+export { icons };
