8de56cc8988e349da1d2afc6d30d055966e4a24f
Dave Batiste <dave.batiste@d2l.com>
2020-09-21 11:15:54 -0400

    Remove extraneous comments.

--- icons.js
+++ icons.js
@@ -9,9 +9,3 @@ Object.keys(icons).forEach((key) => {
 });
 
 export { icons };
-
-This PR enables tinyMCE's built-in resize handle, which is baked into its status-bar at the bottom of the editor.  Unfortunately this means overriding a couple of tinyMCE styles to hide those features.
-
-* enable resize handle (vertical only for now)
-* override status-bar styles to hide the element path and word count
-* override the default resize-handle icon
