950029f64603b51a8dfdb7dfe6b385bc5d210c13
Dave Batiste <dave.batiste@d2l.com>
2020-11-24 09:13:24 -0500

    Add PoC templates for collapsible section and accordion.

--- components/templates.js
+++ components/templates.js
@@ -7,6 +7,7 @@ import '@brightspace-ui/core/components/list/list-item-button.js';
 import '@brightspace-ui/core/components/list/list-item-content.js';
 import 'tinymce/tinymce.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { getUniqueId } from '@brightspace-ui/core/helpers/uniqueId.js';
 import { inputLabelStyles } from '@brightspace-ui/core/components/inputs/input-label-styles.js';
 import { repeat } from 'lit-html/directives/repeat.js';
 import { selectStyles } from '@brightspace-ui/core/components/inputs/input-select-styles.js';
@@ -80,7 +81,6 @@ const templates = [{
 	}, {
 		type: 'create',
 		create: (state) => {
-			console.log(state);
 			return {
 				scripts: [{ src: 'https://code.jquery.com/jquery-3.5.1.slim.min.js', integrity: 'sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' }, { src: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', integrity: 'sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx' }],
 				styleSheets: [{ href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css', integrity: 'sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2' }],
@@ -92,6 +92,102 @@ const templates = [{
 						<p>${state.supportingMessage}</p>
 						${state.dismissable ? `<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>` : ''}
 					</div>`
+			};
+		}
+	}]
+}, {
+	name: 'Collapsible Section (with opener)',
+	description: 'A collapsible container including a button toggle.',
+	steps: [{
+		type: 'properties',
+		getProperties: () => {
+			return [
+				{ key: 'buttonText', type: 'text', label: 'Button Text', default: '' },
+				{ key: 'collapsed', type: 'checkbox', label: 'Collapsed', default: false }
+			]
+		}
+	},{
+		type: 'create',
+		create: (state) => {
+			const targetId = getUniqueId();
+			return {
+				scripts: [{ src: 'https://code.jquery.com/jquery-3.5.1.slim.min.js', integrity: 'sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' }, { src: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', integrity: 'sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx' }],
+				styleSheets: [{ href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css', integrity: 'sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2' }],
+				html: `
+					<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#${targetId}" aria-expanded="${state.collapse ? 'false' : 'true'}" aria-controls="${targetId}">
+						${state.buttonText}
+					</button>
+					<div class="collapse" id="${targetId}" class="${state.collapse ? '' : 'collapse show'}">
+						<div class="card card-body">
+							Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas boatswain schooner gaff booty Jack Tar transom spirits.
+						</div>
+					</div>`
+			};
+		}
+	}]
+}, {
+	name: 'Accordion Container',
+	description: 'An accordion container for one or more panels.',
+	steps: [{
+		type: 'create',
+		create: (state) => {
+			const targetId = getUniqueId();
+			const labelId = getUniqueId();
+			return {
+				scripts: [{ src: 'https://code.jquery.com/jquery-3.5.1.slim.min.js', integrity: 'sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' }, { src: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', integrity: 'sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx' }],
+				styleSheets: [{ href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css', integrity: 'sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2' }],
+				html: `
+					<div class="accordion" id="accordionExample">
+						<div class="card">
+							<div class="card-header" id="${labelId}">
+								<h2 class="mb-0">
+									<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#${targetId}" aria-expanded="true" aria-controls="${targetId}">
+										Accordion Panel Heading
+									</button>
+								</h2>
+							</div>
+							<div id="${targetId}" class="collapse show" aria-labelledby="${labelId}" data-parent="#accordionExample">
+								<div class="card-body">
+								<p>Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker</p>
+								<p>Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.</p>
+								<p>Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas boatswain schooner gaff booty Jack Tar transom spirits.</p>
+								</div>
+							</div>
+						</div>
+						<div>placeholder</div>
+					</div>`
+			};
+		}
+	}]
+}, {
+	name: 'Accordion Panel',
+	description: 'An accordion panel to be placed inside an accordion container.',
+	steps: [{
+		type: 'create',
+		create: (state) => {
+			const targetId = getUniqueId();
+			const labelId = getUniqueId();
+			return {
+				scripts: [{ src: 'https://code.jquery.com/jquery-3.5.1.slim.min.js', integrity: 'sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' }, { src: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', integrity: 'sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx' }],
+				styleSheets: [{ href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css', integrity: 'sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2' }],
+				html: `
+					<div class="card">
+						<div class="card-header" id="${labelId}">
+							<h2 class="mb-0">
+								<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#${targetId}" aria-expanded="false" aria-controls="${targetId}">
+									Accordion Panel Heading
+								</button>
+							</h2>
+						</div>
+						<div id="${targetId}" class="collapse" aria-labelledby="${labelId}" data-parent="#accordionExample">
+							<div class="card-body">
+								<p>Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker</p>
+								<p>Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.</p>
+								<p>Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas boatswain schooner gaff booty Jack Tar transom spirits.</p>
+							</div>
+						</div>
+					</div>
+				`
 			};
 		}
 	}]
